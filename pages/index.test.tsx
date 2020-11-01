5cc8db51d5f466ea4d21154cb87980b0257aa5f4
Paulin Trognon <paulin.trognon@gmail.com>
2020-11-01 17:28:08 +0100

    Add Jest framework + index.tsx test

---
+++ pages/index.test.tsx
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer'
+import IndexPage from '.'
+
+describe('Index page', () => {
+  it('should match the snapshot', () => {
+    const tree = renderer.create(<IndexPage />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
