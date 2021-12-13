509311717c376fd381da18d4ccc06348e2162824
Paulin Trognon <paulin.trognon@enexten.com>
2021-12-13 12:00:21 +0100

    upgrade

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,9 @@
+// lint-staged.config.js
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
-  // Lint and format TypeScript and JS files
+  // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
